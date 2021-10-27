74b2973eca01eb0f0075d76f7e0d6c8eb4546d07
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-27 16:07:28 +0700

    move import to Layout

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,6 @@
-import Head from 'next/head'
-import Navbar from '../comps/Navbar'
-import '../styles/tailwind.css'
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>mohdenny | Home</title>
-        <meta name="keywords" content="Mohdenny Portofolio"/>
-      </Head>
-
-      <Navbar/>
-
       <main className="container">
         
       </main>
