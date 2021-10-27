9d1b73988c5d379e0604745761e0345b6750ee81
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-27 16:06:56 +0700

    import Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Layout from '../components/layout/Layout'
+import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>mohdenny | Home</title>
+        <meta name="keywords" content="Mohdenny Portofolio"/>
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
