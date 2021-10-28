e238729c65e0a1637a692dee87657e14e8786054
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-28 19:34:31 +0700

    import avatar and comp Image

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,16 +1,24 @@
+import Image from 'next/image'
+import avatar from '../../public/images/avatar.jpg'
+
+const data = {
+    headline: 'Hello, my name is Elon Musk. I’m Frontend Developer based in Depok. My Passion is to create an attractive and interactive interface by prioritizing the user experience on Web and Mobile platforms.'
+}
+
 const Hero = () => {
     return (
-        <div id="hero" className="border-4 border-blue-500 h-36rem w-full">
-            <div id="bg-image" className="border-4 border-red-500 h-full w-full">
-            </div>
+        <div id="hero" className="h-34rem w-full">
+            <div id="bg-image" className="bg-space-img h-full w-full"/>
             <div className="flex flex-row transform -translate-y-28rem px-8 w-full">
-                <div id="headline" className="border-4 border-red-500 flex justify-center content-center flex-wrap px-4 h-80 w-full">
-                    <div id="text" className="border-4 border-green-500 h-60 w-full">
+                <div id="headline" className="flex justify-center content-center flex-wrap px-4 h-80 w-full">
+                    <div id="text" className="h-auto w-full">
+                        <p className="text-white text-2xl text-center filter drop-shadow-lg">
+                            {data.headline}
+                        </p>
                     </div>
                 </div>
-                <div id="image-profile" className="border-4 border-red-500 px-4 h-80 w-1/3">
-                    <div id="avatar" className="border-4 border-green-500 h-full w-full">
-                    </div>
+                <div id="image-profile" className="flex justify-center content-center flex-wrap px-4 ml-4 h-80 w-1/3">
+                    <Image id="avatar" className="rounded-xl h-full w-full filter drop-shadow-lg" src={avatar}/>
                 </div>
             </div>
         </div>
