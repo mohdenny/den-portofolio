20c6aa1433330699bb19abb2a75ad5b74891ba5d
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-29 17:57:30 +0700

    import data config

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,9 +1,6 @@
 import Image from 'next/image'
+import { data } from './config'
 import avatar from '../../public/images/avatar.jpg'
-
-const data = {
-    headline: 'Hello, my name is Elon Musk. I’m Frontend Developer based in Depok. My Passion is to create an attractive and interactive interface by prioritizing the user experience on Web and Mobile platforms.'
-}
 
 const Hero = () => {
     return (
@@ -12,13 +9,13 @@ const Hero = () => {
             <div className="flex flex-row transform -translate-y-28rem px-8 w-full">
                 <div id="headline" className="flex justify-center content-center flex-wrap px-4 h-80 w-full">
                     <div id="text" className="h-auto w-full">
-                        <p className="text-white text-2xl text-center filter drop-shadow-lg">
+                        <p className="text-white text-2xl text-center font-roboto-medium">
                             {data.headline}
                         </p>
                     </div>
                 </div>
                 <div id="image-profile" className="flex justify-center content-center flex-wrap px-4 ml-4 h-80 w-1/3">
-                    <Image id="avatar" className="rounded-xl h-full w-full filter drop-shadow-lg" src={avatar}/>
+                    <Image id="avatar" className="rounded-xl h-full w-full" src={avatar}/>
                 </div>
             </div>
         </div>
