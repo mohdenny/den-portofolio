b88145c99d77b6a0312626ff40bbd631ab4fcee2
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-29 17:58:19 +0700

    create components footer

---
+++ components/layout/footer/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <>
+            <div id="footer" className="border-4 border-gray-500 h-24">
+
+            </div>
+        </>
+    )
+}
+
+export default Footer;
