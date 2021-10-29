f649461b142e17f892eae803d95586d84d4e7c02
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-29 17:59:06 +0700

    create component config

---
+++ components/layout/navbar/config.js
@@ -0,0 +1,11 @@
+export const navigation = [
+    { name: 'Home', href: '#', current: true },
+    { name: 'Portofolio', href: '#', current: false },
+    { name: 'Skills', href: '#', current: false },
+    { name: 'Contact', href: '#', current: false },
+    { name: 'About', href: '#', current: false }
+]
+
+export const logo = {
+    text : 'MOHDENNY'
+}
