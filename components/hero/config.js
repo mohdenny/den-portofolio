dd1987dfa6cd05a1d0d799ef9e6b6b7e77afe425
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-29 17:57:17 +0700

    create components config

---
+++ components/hero/config.js
@@ -0,0 +1,3 @@
+export const data = {
+    headline: 'Hello, my name is Mohammad Denny. I’m Frontend Developer based in Depok. My Passion is to create an attractive and interactive interface by prioritizing the user experience on Web and Mobile platforms.'
+}
