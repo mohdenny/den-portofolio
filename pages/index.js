3f9caf24276effee121831ffd0cbcdd994f50e79
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-29 17:59:51 +0700

    import card

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Hero from '../components/hero/Hero'
+import Card from '../components/card/Card'
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <main className="container mx-auto">
-        <div className="border-4 border-green-500 h-96 w-full">
+        <div className="py-4 px-2 h-80 w-full flex flex-row">
+          <Card />
+          <Card margin={'ml-4'}/>
+          <Card margin={'ml-4'}/>
+          <Card margin={'ml-4'}/>
         </div>
       </main>
     </>
