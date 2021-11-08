de3cd93255b0ff58ff4c08186dcdfa872ca06082
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-08 17:42:03 +0700

    delete config

--- pages/contact/config.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const data = [
-    { id: 1, thumb: 'logos/ig.png' ,title: 'Instagram', address: '@mohdenny', url: 'https://www.instagram.com/mohdenny'},
-    { id: 2, thumb: 'logos/github.png', title: 'Github', address: 'mohdenny', url: 'https://github.com/mohdenny'},
-    { id: 3, thumb: 'logos/linkedin.png', title: 'LinkedIn', address: 'mohammad-denny-379065131', url: 'https://www.linkedin.com/in/mohammad-denny-379065131'},
-    { id: 4, thumb: 'logos/gmail.png', title: 'Email', address: 'mohammaddenny94@gmail.com', url: 'https://mail.google.com/mail/u/0/?fs=1&to=mohammaddenny94@gmail.com&tf=cm'}
-]
--- pages/portfolio/config.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const data = [
-    { id: 1, thumb: 'undraw_Landing_page.png', title: 'Lorem ipsum dolor sit amet', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.' },
-    { id: 2, thumb: 'undraw_Landing_page.png', title: 'Lorem ipsum dolor sit amet', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.' },
-    { id: 3, thumb: 'undraw_Landing_page.png', title: 'Lorem ipsum dolor sit amet', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.' },
-    { id: 4, thumb: 'undraw_Landing_page.png', title: 'Lorem ipsum dolor sit amet', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.' },
-    { id: 5, thumb: 'undraw_Landing_page.png', title: 'Lorem ipsum dolor sit amet', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.' }
-]
