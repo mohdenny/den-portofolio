423a3d3ace9c7620871a6d29b0fa2c00708147ec
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-08 17:08:21 +0700

    change loc image

--- components/hero/config.js
+++ components/hero/config.js
@@ -1,4 +1,4 @@
 export const data = {
     headline: 'Hello, my name is Mohammad Denny. I’m Frontend Developer based in Depok. My Passion is to create an attractive and interactive interface by prioritizing the user experience on Web and Mobile platforms.',
-    image: 'image-profile.jpg'
+    image: 'profile/image-profile.jpg'
 }
