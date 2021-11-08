ae6c67c4bbb06a7825c803cc958919cccddf2b2b
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-08 17:07:58 +0700

    add logos and url

--- pages/contact/config.js
+++ pages/contact/config.js
@@ -1,6 +1,6 @@
 export const data = [
-    { id: 1, thumb: 'undraw_Landing_page.png', title: 'Instagram', url: '@mohdenny'},
-    { id: 2, thumb: 'undraw_Landing_page.png', title: 'Github', url: 'https://github.com/mohdenny'},
-    { id: 3, thumb: 'undraw_Landing_page.png', title: 'LinkedIn', url: 'https://www.linkedin.com/in/mohammad-denny-379065131'},
-    { id: 4, thumb: 'undraw_Landing_page.png', title: 'Email', url: 'mohammaddenny94@gmail.com'}
+    { id: 1, thumb: 'logos/ig.png' ,title: 'Instagram', address: '@mohdenny', url: 'https://www.instagram.com/mohdenny'},
+    { id: 2, thumb: 'logos/github.png', title: 'Github', address: 'mohdenny', url: 'https://github.com/mohdenny'},
+    { id: 3, thumb: 'logos/linkedin.png', title: 'LinkedIn', address: 'mohammad-denny-379065131', url: 'https://www.linkedin.com/in/mohammad-denny-379065131'},
+    { id: 4, thumb: 'logos/gmail.png', title: 'Email', address: 'mohammaddenny94@gmail.com', url: 'https://mail.google.com/mail/u/0/?fs=1&to=mohammaddenny94@gmail.com&tf=cm'}
 ]
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,14 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import { data } from './config'
 import { motion } from 'framer-motion'
 import { squareVariants } from '../../helpers/variants'
-import Image from 'next/image'
 import Header from '../../components/header/Header'
 
 
 const Contact = () => {
     return (
         <>
-            <main className="container mx-auto mt-4 h-full w-full">
+            <main className="container mx-auto mt-4 min-h-screen w-full">
                 <div className="flex justify-center">
                     <Header text={'Contact'} />
                 </div>
@@ -38,14 +39,18 @@ const Contact = () => {
                                 >
                                     <div id="content" className="h-full w-1/2">
                                         <div className="h-1/2 flex items-center justify-center">
-                                            <p className="text-white text-center text-xl font-roboto-medium">{item.title}</p>
+                                            <Link href={item.url}>
+                                                <a target="_blank" rel="noopener noreferrer">
+                                                    <p className="text-white text-center text-xl font-roboto-medium">{item.title}</p>
+                                                </a>
+                                            </Link>
                                         </div>
                                         <div className="flex items-center justify-center bg-gray-800 rounded-b-xl h-1/2 px-4">
-                                            <p className="text-white text-center text-sm font-roboto-regular">{item.url}</p>
+                                            <p className="text-white text-center text-sm font-roboto-regular">{item.address}</p>
                                         </div>
                                     </div>
-                                    <div id="card-thumb" className="flex items-center justify-center h-full w-1/2">
-                                        <Image src={require(`../../public/images/${item.thumb}`)} alt="logo-skill" className="object-contain"/>
+                                    <div id="card-thumb" className="flex items-center justify-center bg-white h-full w-1/2">
+                                        <Image src={require(`../../public/images/${item.thumb}`)} alt="logo-skill" height="150" width="100%" className="object-contain drop-shadow-lg"/>
                                     </div>
                                 </motion.div>
                             )
