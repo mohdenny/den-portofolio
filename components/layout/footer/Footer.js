391eb9a61d4690e5e8b49edcb2a473347ee6ac1a
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-10 16:38:53 +0700

    change flex position

--- components/layout/footer/Footer.js
+++ components/layout/footer/Footer.js
@@ -2,12 +2,11 @@ const Footer = () => {
     return (
         <>
             <div id="footer" className="mt-4 h-40 w-full py-2 flex items-center justify-between px-10 text-white font-roboto-regular">
-                <div>
-                    <p className="text-2xl">Mohammad Denny</p>
-                    <p className="text-sm">Frontend Developer</p>
+                <div className="flex items-center justify-start h-full w-full">
+                    <p className="text-2xl">Mohammad Denny<p className="text-sm br">Frontend Developer</p></p>
                 </div>
-                <div>
-                    <p className="text-xs">Copyright 2021 Mohammad Denny</p>
+                <div className="flex items-center justify-end text-right h-full w-full">
+                    <p className="text-xs">Copyright © 2021<p className="text-sm br">Mohammad Denny</p></p>
                 </div>
             </div>
         </>
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -9,7 +9,7 @@ const data = [
     { id: 1, thumb: 'logos/ig.png' ,title: 'Instagram', address: '@mohdenny', url: 'https://www.instagram.com/mohdenny'},
     { id: 2, thumb: 'logos/github.png', title: 'Github', address: 'mohdenny', url: 'https://github.com/mohdenny'},
     { id: 3, thumb: 'logos/linkedin.png', title: 'LinkedIn', address: 'mohammad-denny-379065131', url: 'https://www.linkedin.com/in/mohammad-denny-379065131'},
-    { id: 4, thumb: 'logos/gmail.png', title: 'Email', address: 'mohammaddenny94@gmail.com', url: 'https://mail.google.com/mail/u/0/?fs=1&to=mohammaddenny94@gmail.com&tf=cm'},
+    { id: 4, thumb: 'logos/gmail.png', title: 'Email', address: 'mohammaddenny94\n@gmail.com', url: 'https://mail.google.com/mail/u/0/?fs=1&to=mohammaddenny94@gmail.com&tf=cm'},
     { id: 4, thumb: 'logos/twitter.png', title: 'Twitter', address: '@mohdenny', url: 'https://twitter.com/mohdenny'},
     { id: 4, thumb: 'logos/youtube.png', title: 'Youtube', address: 'Mohammad Denny', url: 'https://www.youtube.com/channel/UCvfQmEp-sxwtGbSqfzyOe4g'}
 ]
@@ -49,7 +49,7 @@ const Contact = () => {
                                         <div className="h-1/2 flex items-center justify-center">
                                             <Link href={item.url}>
                                                 <a target="_blank" rel="noopener noreferrer">
-                                                    <p className="text-white text-center text-xl font-roboto-medium">{item.title}</p>
+                                                    <p className="text-white text-center text-xl font-roboto-medium hover:text-gray-300">{item.title}</p>
                                                 </a>
                                             </Link>
                                         </div>
