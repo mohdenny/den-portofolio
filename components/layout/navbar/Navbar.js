ff2430dd2767248b23e45e158d6f9a505efaaabf
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-13 08:56:03 +0700

    replace button with anchor

--- components/layout/navbar/Navbar.js
+++ components/layout/navbar/Navbar.js
@@ -83,7 +83,7 @@ export default function Navbar() {
                                         {navigation.map((item) => (
                                         <Link key={item.id} href={item.href}>
                                             <motion.a
-                                                href={item.href}
+                                                href=''
                                                 className={classNames(
                                                 router.asPath === item.href ? 'bg-gray-900 text-white font-roboto-medium font-bold' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                                                 'px-3 py-2 rounded-md text-sm font-roboto-regular'
@@ -105,18 +105,20 @@ export default function Navbar() {
                     <Disclosure.Panel className="sm:hidden">
                         <div className="px-2 pt-2 pb-3 space-y-1">
                         {navigation.map((item) => (
-                            <Disclosure.Button
-                                key={item.id}
-                                as="a"
-                                href={item.href}
-                                className={classNames(
-                                    router.asPath === item.href ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                    'block px-3 py-2 rounded-md text-base font-medium'
-                                )}
-                                aria-current={router.asPath === item.href ? 'page' : undefined}
-                            >
-                            {item.name}
-                            </Disclosure.Button>
+                            <Link key={item.id} href={item.href}>
+                                <Disclosure>
+                                    <a
+                                        href=''
+                                        className={classNames(
+                                            router.asPath === item.href ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'block px-3 py-2 rounded-md text-base font-medium'
+                                        )}
+                                        aria-current={router.asPath === item.href ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </a>
+                                </Disclosure>
+                            </Link>
                         ))}
                         </div>
                     </Disclosure.Panel>
