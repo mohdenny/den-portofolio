ea2be136bebb52118e428e23c283d205532bc8f1
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-15 20:37:19 +0700

    set padding

--- components/articlebox/Title.js
+++ components/articlebox/Title.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Title = ({ data }) => {
     return (
-        <div id="title" className="flex items-center justify-center text-justify px-2 h-1/2 w-full">
+        <div id="title" className="flex items-center justify-center text-justify px-2 py-2 h-1/4 w-full">
             <p className="text-white text-xl font-roboto-medium hover:text-gray-300">
                 <Link href='#'><a>{data.title}</a></Link>
             </p>
