b4ab7765456e9451c937cc4526acbaa847aa5bb4
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-15 20:36:14 +0700

    remove motion

--- components/articlebox/ArticleBox.js
+++ components/articlebox/ArticleBox.js
@@ -1,29 +1,17 @@
 import Title from './Title'
 import Desc from './Desc'
 import Thumb from './Thumb'
-import { motion } from 'framer-motion'
 
 const ArticleBox = ({ data }) => {
     return (
         <div>
-            <motion.div 
-                className="border-4 flex flex-col bg-gray-700 h-full w-full rounded-xl"
-                whileHover={{ 
-                    scale: 1.1 , 
-                    textShadow: "0px 0px 8px rgb(0, 0, 0)",
-                    boxShadow: "0px 0px 8px rgb(0, 0, 0)" 
-                }}
-                transition={{ 
-                    type: "spring", 
-                    stiffness: 300 
-                }}
-            >
+            <div className="flex flex-col bg-gray-700 h-full w-full rounded-xl">
                 <Thumb data={data} />
-                <div id="content" className="border-4 h-full w-full">
+                <div id="content" className="h-1/3 w-full">
                     <Title data={data}/>
                     <Desc data={data}/>
                 </div>
-            </motion.div>
+            </div>
         </div>
     )
 }
