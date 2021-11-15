04cb7d1640cea401ca8803057512baa88dc31435
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-15 20:36:41 +0700

    set padding and rounded

--- components/articlebox/Desc.js
+++ components/articlebox/Desc.js
@@ -1,6 +1,6 @@
 const Desc = ({ data }) => {
     return (
-        <div id="desc" className="flex items-center justify-center text-justify break-all bg-gray-800 rounded-br-xl overflow-ellipsis overflow-hidden px-2 h-1/2 w-full">
+        <div id="desc" className="flex items-center justify-center text-justify break-all bg-gray-800 rounded-b-xl overflow-ellipsis overflow-hidden px-4 py-4 h-2/2 w-full">
             <p className="text-white text-sm font-roboto-regular">{data.explanation}</p>
         </div>
     )
