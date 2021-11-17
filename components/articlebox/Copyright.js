2193e4752df89ff075888bcf3a575fc9944b7f2e
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-17 17:02:29 +0700

    del transform rotate

--- components/articlebox/Copyright.js
+++ components/articlebox/Copyright.js
@@ -1,7 +1,7 @@
 const Copyright = ({ data }) => {
     return (
         <div className="flex justify-end  w-full h-full">
-            <p className="bg-gray-900 py-2 rounded-xl flex items-center w-30 h-full transform rotate-2 text-gray-300 text-xs px-4">
+            <p className="bg-gray-900 py-2 rounded-xl flex items-center w-30 h-full text-gray-300 text-xs px-4">
                 Copyright: {data.copyright}
             </p>
         </div>
