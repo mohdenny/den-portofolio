82cfd92a2433c10041c487fd322d1eff6d4386cf
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-17 16:40:06 +0700

    update conditional rendering

--- components/articlebox/Desc.js
+++ components/articlebox/Desc.js
@@ -1,7 +1,12 @@
+import Copyright from "./Copyright"
+
 const Desc = ({ data }) => {
     return (
-        <div id="desc" className="flex items-center justify-center text-justify break-all bg-gray-800 rounded-b-xl overflow-ellipsis overflow-hidden px-4 py-4 h-2/2 w-full">
+        <div id="desc" className="flex flex-col items-center justify-center text-justify break-all bg-gray-800 rounded-b-xl overflow-ellipsis overflow-hidden px-4 py-4 h-2/2 w-full">
             <p className="text-white text-sm font-roboto-regular">{data.explanation}</p>
+            {
+                data.copyright && <Copyright data={data} />
+            }
         </div>
     )
 }
