294308f8bb4621d991f4e9065bcdcdaa6308a162
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-17 17:01:11 +0700

    set layout objectfit image

--- components/hero/Image.js
+++ components/hero/Image.js
@@ -5,12 +5,12 @@ import { photoProfileVariants } from '../../helpers/variants'
 const HeroImage = ({ src }) => {return (
         <motion.div 
             id="image" 
-            className="flex justify-center content-center flex-wrap px-4 lg:w-1/3 h-80 overflow-hidden w-full"
+            className="flex justify-center content-center flex-wrap px-4 lg:w-1/3 h-80 overflow-hidden relative w-full"
             variants={photoProfileVariants}
             initial="initial"
             animate="animate"
         >
-            <Image id="image-profile" className="object-cover object-center" src={require(`../../public/images/${src.image}`)}/>
+            <Image id="image-profile" src={require(`../../public/images/${src.image}`)} layout="fill" objectFit="contain"/>
         </motion.div>
     )
 }
