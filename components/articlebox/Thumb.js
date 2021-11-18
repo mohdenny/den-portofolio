ce054934a0867826a462f331d91f032f0d66e0d2
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-18 18:38:40 +0700

    del div parent

--- components/articlebox/Thumb.js
+++ components/articlebox/Thumb.js
@@ -3,22 +3,20 @@ import Image from 'next/image'
 const Thumb = ({ data }) => {
     return (
         <>
-            <div className="flex h-96 w-full content-center relative justify-center">
-                {
-                    data.media_type == 'image' ?
-                        <Image src={data.hdurl} layout="fill" objectFit="cover" />
-                    :
-                    <iframe
-                        title="space-video"
-                        src={data.url}
-                        frameBorder="0"
-                        gesture="media"
-                        allow="encrypted-media"
-                        allowFullScreen
-                        className="h-full w-full"
-                    />
-                }
-            </div>
+            {
+                data.media_type == 'image' ?
+                    <Image src={data.hdurl} layout="fill" objectFit="cover" />
+                :
+                <iframe
+                    title="space-video"
+                    src={data.url}
+                    frameBorder="0"
+                    gesture="media"
+                    allow="encrypted-media"
+                    allowFullScreen
+                    className="h-full w-full"
+                />
+            }
         </>
     )
 }
