03749d2386b025aeec1d5acdc3e45d7dea762c5f
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-26 15:01:09 +0700

    change background color

--- components/card/Thumb.js
+++ components/card/Thumb.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const CardThumb = ({ data }) => {
     return (
-        <div id="card-thumb" className="bg-white overflow-hidden h-1/2 w-full relative">
+        <div id="card-thumb" className="bg-gray-300 overflow-hidden h-1/2 w-full relative">
           <Image src={require(`../../public/images/${data.thumb}`)} objectFit="contain" layout="fill" />
         </div>
     )
