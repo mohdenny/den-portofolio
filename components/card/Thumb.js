267d97816f56ffc816df6e3c1bf67540a6d33761
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-26 10:26:42 +0700

    add classname background color

--- components/card/Thumb.js
+++ components/card/Thumb.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 const CardThumb = ({ data }) => {
     return (
-        <div id="card-thumb" className="overflow-hidden h-1/2 w-full">
-          <Image src={require(`../../public/images/${data.thumb}`)} className="object-contain object-center" />
+        <div id="card-thumb" className="bg-white overflow-hidden h-1/2 w-full relative">
+          <Image src={require(`../../public/images/${data.thumb}`)} objectFit="contain" layout="fill" />
         </div>
     )
 }
