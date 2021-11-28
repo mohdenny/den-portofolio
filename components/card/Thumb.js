5437d0cf43e8eb4ae5cded6b17297c8397958fe4
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-28 09:06:25 +0700

    change object to cover

--- components/card/Thumb.js
+++ components/card/Thumb.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const CardThumb = ({ data }) => {
     return (
         <div id="card-thumb" className="bg-gray-300 overflow-hidden h-1/2 w-full relative">
-          <Image src={require(`../../public/images/${data.thumb}`)} objectFit="contain" layout="fill" />
+          <Image src={require(`../../public/images/${data.thumb}`)} objectFit="cover" layout="fill" />
         </div>
     )
 }
