f87ead2fbb052c72cb35868bfd225f8e0100a276
Mohammad Denny <mohammaddenny94@gmail.com>
2021-11-28 09:06:46 +0700

    add data portfolio

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -5,11 +5,11 @@ import Card from '../../components/card/Card'
 import { squareVariants } from '../../helpers/variants'
 
 export const data = [
+  { thumb: 'Bisnis.com-Spirit-Indonesia-thumb.png', title: 'Bisnis.com (Spirit Indonesia!)', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.', url: 'https://blissful-perlman-ba1151.netlify.app/' },
   { thumb: 'aligbola-thumb.png', title: 'Aligbola', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.', url: 'https://alig-bola.web.app/' },
   { thumb: 'gitrepos-thumb.png', title: 'Gitrepos', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.', url: 'https://infallible-poincare-6ad975.netlify.app/' },
-  { thumb: 'undraw_Landing_page.png', title: 'Lorem ipsum dolor sit amet', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.', url: '' },
+  { thumb: 'laparcom-thumb.png', title: 'Laparcom', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.', url: 'https://adoring-keller-e29a10.netlify.app/' },
   { thumb: 'porto-semantic-thumb.png', title: 'Semantic Web Portfolio', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.', url: 'https://eloquent-murdock-69203b.netlify.app/' },
-  { thumb: 'Bisnis.com-Spirit-Indonesia-thumb.png', title: 'Bisnis.com (Spirit Indonesia!)', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a libero quam. Nam pellentesque, mi vulputate rhoncus laoreet, mi nunc commodo nulla, ut convallis purus eros nec massa.', url: 'https://blissful-perlman-ba1151.netlify.app/' }
 ]
 
 const Portfolio = () => {
