5bd330612db849a32245e5320aeb35671f059bbb
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-09 12:23:19 +0700

    set break-words to desc

--- components/articlebox/Desc.js
+++ components/articlebox/Desc.js
@@ -3,7 +3,7 @@ import Copyright from "./Copyright"
 const Desc = ({ data }) => {
     return (
         <div id="desc" className="flex flex-col items-center justify-center space-y-2 text-justify break-all bg-gray-800 rounded-b-xl overflow-ellipsis overflow-hidden px-4 py-4 h-2/2 w-full">
-            <p className="text-white text-sm font-roboto-regular">{data.explanation}</p>
+            <p className="text-white text-sm font-roboto-regular break-words">{data.explanation}</p>
             {
                 data.copyright && <Copyright data={data} />
             }
