a9886365f70a59502d162584a80b957abc858129
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-06 01:20:42 +0700

    change to break words

--- components/articlebox/Desc.js
+++ components/articlebox/Desc.js
@@ -2,8 +2,8 @@ import Copyright from "./Copyright"
 
 const Desc = ({ data }) => {
     return (
-        <div id="desc" className="flex flex-col items-center justify-center space-y-2 text-justify break-all bg-gray-800 rounded-b-xl overflow-ellipsis overflow-hidden px-4 py-4 h-2/2 w-full">
-            <p className="text-white text-sm font-poppins-regular break-words">{data.explanation}</p>
+        <div id="desc" className="flex flex-col items-center justify-center space-y-2 text-justify break-words bg-gray-800 rounded-b-xl overflow-ellipsis overflow-hidden px-4 py-4 h-2/2 w-full">
+            <p className="text-white text-sm font-poppins-regular">{data.explanation}</p>
             {
                 data.copyright && <Copyright data={data} />
             }
