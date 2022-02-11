635dc10f33a74e98102246026c5ff96fbbb3d60a
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-11 11:33:55 +0700

    change title and caption

--- components/hero/config.js
+++ components/hero/config.js
@@ -1,5 +1,7 @@
 export const data = {
-    title: 'Hello, my name is Mohammad Denny',
-    caption: 'I really like to explore Frontend Developer field and Im also interested in UI/UX design.',
+    title1: 'Hello, my name is',
+    title2: 'Mohammad Denny',
+    caption1: 'I really like to explore Frontend Developer field',
+    caption2: 'and Im also interested in UI/UX design.',
     image: 'profile/image-profile.jpg'
 }
