5511dbb8eb19569f1207f895c0713d26c2190eda
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-13 15:13:42 +0700

    change width image

--- components/hero/Image.js
+++ components/hero/Image.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 import { photoProfileVariants } from '../../helpers/variants'
 
 const HeroImage = ({ src }) => {return (
-        <motion.div className='flex justify-center content-center mx-auto flex-wrap bg-gradient-to-tr from-boston-blue-100 to-white lg:w-1/2 w-full rounded-full p-2 h-avatar-border'
+        <motion.div className='flex justify-center content-center mx-auto flex-wrap bg-gradient-to-tr from-boston-blue-100 to-white w-auto rounded-full p-2 h-avatar-border'
             variants={photoProfileVariants}
             initial="initial"
             animate="animate"
@@ -13,7 +13,7 @@ const HeroImage = ({ src }) => {return (
                 className="flex justify-center content-center h-avatar-image overflow-hidden relative w-full border-8 rounded-full"
                 
             >
-                <Image id="image-profile" src={require(`../../public/images/${src.image}`)} layout="fill" objectPosition="center" objectFit="cover"/>
+                <Image id="image-profile" src={require(`../../public/images/${src.image}`)} width="500px" height="100%" objectPosition="center" objectFit="cover"/>
             </div>
         </motion.div>
     )
